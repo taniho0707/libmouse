04da690a3ca919364ad5fcd0ca1ff9d27e50902d
taniho_0707 <taniho_0707@yahoo.co.jp>
2017-10-12 22:08:13 +0900

    グラフをとりあえず使えるように

--- Node.h
+++ Node.h
@@ -9,22 +9,28 @@
 
 class Node{
 private:
-	static const uint16_t MAX = 65535;
-	
-	uint16_t num;
-	std::vector<Node *> edges_to;
-	std::vector<uint16_t> edges_cost;
-
-	bool done; // 確定ノードか否か
-	uint16_t cost; // このノードへの現時点で判明している最小コスト
 
 public:
-	Node(uint16_t n);
+	static const uint16_t MAX = 65535;
+
+	std::vector<uint16_t> edges_to;
+	std::vector<uint16_t> edges_cost;
+
+	// for Dijkstra
+	bool done; // 確定ノードか否か
+	uint16_t cost; // このノードへの現時点で判明している最小コスト
+	uint16_t from; // このノードへ接続したノード番号
+
+	Node();
 	
-	std::vector<Node *> getConnectedNodes();
-	bool isConnectedNode(Node&);
-	uint16_t getCost(Node *);
-	uint16_t getNumber();
+	/* std::vector<uint16_t> getConnectedNodes(); */
+	/* bool isConnected(uint16_t num); */
+	/* uint16_t getCost(uint16_t num); */
+
+	bool operator< (const Node& input) const;
+	bool operator> (const Node& input) const;
+	bool operator< (const Node* input) const;
+	bool operator> (const Node* input) const;
 	
 	~Node();
 };
